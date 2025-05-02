efdad1e2c1e4de2331d943f028a4e0cdefb02840
Kusuma <rajukusuma@yahoo.com>
2025-05-02 15:50:03 +0530

    Creating Repository for StockItem

--- StockItemManagementService/src/main/java/com/wimo/repository/StockItemRepositry.java
+++ StockItemManagementService/src/main/java/com/wimo/repository/StockItemRepositry.java
@@ -1,5 +1,9 @@
 package com.wimo.repository;
 
-public interface StockItemRepositry {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wimo.model.StockItem;
+
+public interface StockItemRepositry extends JpaRepository<StockItem,Integer> {
 
 }
