1d52859adc5e66b49dd3e12de9af64aa1f2b5143
Kusuma <rajukusuma@yahoo.com>
2025-05-02 15:41:22 +0530

    Adding Data Members into the StockItem entity

--- StockItemManagementService/src/main/java/com/wimo/model/StockItem.java
+++ StockItemManagementService/src/main/java/com/wimo/model/StockItem.java
@@ -1,13 +1,20 @@
 package com.wimo.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class StockItem {
 	 @Id
-	 private int itemId;
-	 private String itemName;
-	 private String	itemCategory;
-	 private int quantity;
+	 private int stockId;
+	 private String stockName;
+	 private String	stockCategory;
+	 private int stockQuantity;
 	 private int zoneId;
 
 }
