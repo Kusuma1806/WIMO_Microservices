7aebcdb568145265a6d61e1704178e2a36df08b1
Kusuma <rajukusuma@yahoo.com>
2025-05-04 12:23:18 +0530

    Declaring the methods for StockItem Entity

--- StockItemManagementService/src/main/java/com/wimo/service/StockItemService.java
+++ StockItemManagementService/src/main/java/com/wimo/service/StockItemService.java
@@ -1,5 +1,23 @@
 package com.wimo.service;
 
+import java.util.List;
+
+import com.wimo.model.StockItem;
+
 public interface StockItemService {
+	public abstract String saveStockItem(StockItem stockItem);
+
+	public abstract StockItem updateStockItem(StockItem stockItem);
+
+	public abstract String removeStockItem(int stockId);
+
+	public abstract StockItem getStockItemById(int stockId);
+
+	public abstract List<StockItem> getAllStockItems();
+	
+	public abstract List<StockItem> findByStockCategoryIs(String stockCategory);
+	
+	public abstract List<StockItem> findByZoneIdIs(String zoneId);
+	
 
 }
